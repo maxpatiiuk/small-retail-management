c6aa7428c8f16d4aeabefdd20f164c1b6e1b2c5c
Max Patiiuk <max@patii.uk>
2023-07-04 15:02:40 -0700

    chore: Add next.js boilerplate

---
+++ const/localization.ts
@@ -0,0 +1,31 @@
+/**
+ * Until there is need to provide localization for other languages,
+ * all localizable strings would be stored in one place. This helps with
+ * consistency and reusability.
+ */
+
+export const localization = {
+  siteTitle: 'Max Patiiuk',
+  siteDescription: `Full Stack Web Developer`,
+  siteKeywords: `
+    Max Patiiuk, Maksym Patiiuk, Maksym Patiiuk CV, Maksym Patiiuk portfolio,
+    mambo shop, mambo, В гостях у MAMBO, мамбо, mambo experimental,
+    Максим Патіюк
+  `,
+  siteAuthor: 'Max Patiiuk',
+  siteAuthorTitle: 'SDE II at Esri',
+  myProjects: 'My projects',
+  yes: 'Yes',
+  no: 'No',
+  returnToHomePage: 'Return to homepage',
+  viewOnGitHub: 'View this project on GitHub',
+  unexpectedError: 'Unexpected Error',
+  reload: 'Reload',
+  previousPage: 'Previous page',
+  unexpectedErrorHasOccurred: 'An unexpected error has occurred.',
+  notFound: 'Oops! Nothing was found',
+  notFoundDescription: `
+    The page you are looking for might have been removed,
+    had its name changed or is temporarily unavailable.
+  `,
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,40 @@
+import 'tailwindcss/tailwind.css';
+
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import React from 'react';
+
+import { pageView } from '../lib/googleAnalytics';
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  const { events } = useRouter();
+
+  React.useEffect(() => {
+    if ('serviceWorker' in navigator)
+      window.addEventListener(
+        'load',
+        () => void navigator.serviceWorker.register('/sw.js')
+      );
+  }, []);
+
+  React.useEffect(() => {
+    events.on('routeChangeComplete', pageView);
+    return (): void => events.off('routeChangeComplete', pageView);
+  }, [events]);
+
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+const app = App;
+export default app;
