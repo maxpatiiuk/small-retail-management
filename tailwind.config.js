4f7334ddff5f65caf4dfc4819fa4167b33b0beb9
Max Patiiuk <max@patii.uk>
2023-07-04 15:00:11 -0700

    chore: Add boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * This config partially overwrites and extends the default Tailwind config:
+ * https://github.com/tailwindlabs/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  corePlugins: {
+    // Disable unneeded components to reduce performance impact
+    float: false,
+    clear: false,
+    skew: false,
+    caretColor: false,
+    sepia: false,
+    // Don't want to bother with adding back styles:
+    preflight: false,
+  },
+  darkMode: 'media',
+  plugins: [require('@tailwindcss/forms')],
+};
